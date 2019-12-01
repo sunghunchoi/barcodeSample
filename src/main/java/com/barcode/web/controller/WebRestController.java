bc1ed2851f5b9d6efa69f98ceebc9df5cb7da5b9
sunghunchoi <csh6222sp@gmail.com>
2019-12-01 21:07:53 +0900

    add controller and domain

--- src/main/java/com/barcode/web/controller/WebRestController.java
+++ src/main/java/com/barcode/web/controller/WebRestController.java
@@ -1,13 +1,34 @@
 package com.barcode.web.controller;
 
+import com.barcode.web.domain.posts.PostsRepository;
+import com.barcode.web.dto.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello(){
         return "HelloWorld";
     }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+
+        System.out.println("Post posts !");
+        System.out.println("Post title : " + dto.getTitle());
+        System.out.println("Post content : " + dto.getContent());
+        System.out.println("Post author : " + dto.getAuthor());
+        postsRepository.save(dto.toEntity()
+
+        );
+    }
+
 }
---
+++ src/main/java/com/barcode/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,24 @@
+package com.barcode.web.dto;
+
+import com.barcode.web.domain.posts.Posts;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
